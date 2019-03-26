e78c225bb2e5b03fbee318bd7b126249aa5b649e
marija19280 <marija19280@gmail.com>
2019-03-26 17:45:36 +0100

    hhg

---
+++ PIS-lab1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PIS_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
